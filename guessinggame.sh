7fcaeb95f88c9868ecfc2520b03a5c2f0b692274
4M-1N3 <m.alloul.cad@hotmail.com>
2020-08-28 16:07:27 +0100

    Added some comments to guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,20 @@
+#!/user/bin/env bash
+# File guessinggame.sh
+
+# A script that ask continuously the user to guess the number of files in the current directory# 
+
+
 echo "how many files are in the current directory ?"
 read response
 
-n=$(ls -A | egrep [^guessinggame.sh] | wc -l)
+n=$(ls -A | egrep [^guessinggame.sh] | wc -l)         # n= number of files in the current directory except the guessinggame.sh #
 
 while [[ $response != $n ]]
 do
-	function evaluate {
+	function evaluate {                               # Function that determin either if the response given by the user is a digit or not #
 	if [[ $response =~ ^[0-9]+$ ]]
-	then
-		value=1
+	then                                              # value = 1 if $response is a digit #
+		value=1                                   #       = 0 if $response is not a digit # 
 	else
 		value=0
 	fi
@@ -29,6 +35,7 @@ do
 	fi
 
 	echo "Lets try again"
+	echo
 	read response
 done
 
